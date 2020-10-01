ed3a4f944914771358cdc56f6751e2e43d2114e5
Tatsiana Tatartsava <tatsiana.tatartsava@gmail.com>
2020-10-01 19:29:23 +1000

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) { return 0; } else {
+        function reduceFunc(minNum, num) {
+            if (minNum >= num) { minNum = num; };
+            return minNum;
+        }
+        return array.reduce(reduceFunc);
+    };
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) { return 0; } else {
+        function reduceFunc(maxNum, num) {
+            if (maxNum <= num) { maxNum = num; };
+            return maxNum;
+        }
+        return array.reduce(reduceFunc);
+    };
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) { return 0; } else {
+        function reduceFunc(sum, num) {
+            return (sum + num);
+        }
+        return (array.reduce(reduceFunc) / array.length);
+    };
 }
